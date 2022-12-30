7c52c2ecdbad35f43764648f768d2933b5464d9d
Jacob Quinn <quinn.jacobd@gmail.com>
2022-12-30 10:43:08 -0700

    More tests

--- test/example_test.jl
+++ test/example_test.jl
@@ -1,202 +1,63 @@
-#  Copyright 2016 The Go Authors. All rights reserved.
-#  Use of this source code is governed by a BSD-style
-#  license that can be found in the LICENSE file.
-module flate_test
-
 #  In performance critical applications, Reset can be used to discard the
 #  current compressor or Decompressor state and reinitialize them quickly
 #  by taking advantage of previously allocated memory.
-function Example_reset()
-    proverbs = Vector{String}[
-        "Don't communicate by sharing memory, share memory by communicating.\\n",
-        "Concurrency is not parallelism.\\n",
-        "The bigger the interface, the weaker the abstraction.\\n",
-        "Documentation is for users.\\n",
+@testset "Example_reset" begin
+    proverbs = [
+        "Don't communicate by sharing memory, share memory by communicating.\n",
+        "Concurrency is not parallelism.\n",
+        "The bigger the interface, the weaker the abstraction.\n",
+        "Documentation is for users.\n",
     ]
 
-    local r::strings.Reader
-
-    local b::bytes.Buffer
-
-    buf = make(Vector{UInt8}, 32 << 10)
-    zw, err = flate.NewWriter(nothing, flate.DefaultCompression)
-    if err !== nothing
-        error(err)
-    end
-
-    zr = flate.NewReader(nothing)
-    for (_, s) in proverbs
-        r.Reset(s)
-        b.Reset()
+    b = IOBuffer()
+    zw = Flate.NewWriter(IOBuffer(), Flate.DefaultCompression)
+    zr = Flate.NewReader(IOBuffer())
+    for s in proverbs
+        take!(b)
         #  Reset the compressor and encode from some input stream.
-        zw.Reset(b)
-        if (_, err = io.CopyBuffer(zw, r, buf); err !== nothing)
-            error(err)
-        end
-
-        if (err = zw.Close(); err !== nothing)
-            error(err)
-        end
-
-        #  Reset the Decompressor and decode to some output stream.
-        if (err = (zr::flate.Resetter).Reset(b, nothing); err !== nothing)
-            error(err)
-        end
-
-        if (_, err = io.CopyBuffer(os.Stdout, zr, buf); err !== nothing)
-            error(err)
-        end
-
-        if (err = zr.Close(); err !== nothing)
-            error(err)
-        end
-
+        reset(zw, b)
+        write(zw, Go.Slice(s))
+        close(zw)
+        seekstart(b)
+        zr = reset(zr, b, Go.Slice(UInt8, 0))
+        out = sprint(write, zr)
+        @test out == s
     end
-
 end
+
 #  A preset dictionary can be used to improve the compression ratio.
 #  The downside to using a dictionary is that the compressor and Decompressor
 #  must agree in advance what dictionary to use.
-function Example_dictionary()
+@testset "Example_dictionary" begin
     #  The data to compress should (but is not required to) contain frequent
     #  The dictionary is a string of bytes. When compressing some input data,
     #  the compressor will attempt to substitute substrings with matches found
     #  in the dictionary. As such, the dictionary should only contain substrings
     #  that are expected to be found in the actual data stream.
     #  substrings that match those in the dictionary.
-    const dict =
-        "<?xml version=\"1.0\"?>" + "<book>" + "<data>" + "<meta name=\"" + "\" content=\""
-
-    const data = "<?xml version=\"1.0\"?>\n<book>\n\t<meta name=\"title\" content=\"The Go Programming Language\"/>\n\t<meta name=\"authors\" content=\"Alan Donovan and Brian Kernighan\"/>\n\t<meta name=\"published\" content=\"2015-10-26\"/>\n\t<meta name=\"isbn\" content=\"978-0134190440\"/>\n\t<data>...</data>\n</book>\n"
-
+    dict = "<?xml version=\"1.0\"?>" * "<book>" * "<data>" * "<meta name=\"" * "\" content=\""
+    data = "<?xml version=\"1.0\"?>\n<book>\n\t<meta name=\"title\" content=\"The Go Programming Language\"/>\n\t<meta name=\"authors\" content=\"Alan Donovan and Brian Kernighan\"/>\n\t<meta name=\"published\" content=\"2015-10-26\"/>\n\t<meta name=\"isbn\" content=\"978-0134190440\"/>\n\t<data>...</data>\n</book>\n"
     #  Compress the data using the specially crafted dictionary.
-    local b::bytes.Buffer
-
-    zw, err = flate.NewWriterDict(b, flate.DefaultCompression, convert(Vector{UInt8}, dict))
-    if err !== nothing
-        error(err)
-    end
-
-    if (_, err = io.Copy(zw, strings.NewReader(data)); err !== nothing)
-        error(err)
-    end
-
-    if (err = zw.Close(); err !== nothing)
-        error(err)
-    end
-
+    b = IOBuffer()
+    zw = Flate.NewWriterDict(b, Flate.DefaultCompression, Go.Slice(dict))
+    write(zw, Go.Slice(data))
+    close(zw)
+    seekstart(b)
     #  The Decompressor must use the same dictionary as the compressor.
     #  Otherwise, the input may appear as corrupted.
-    println("Decompressed output using the dictionary:")
-    zr = flate.NewReaderDict(bytes.NewReader(b.Bytes()), convert(Vector{UInt8}, dict))
-    if (_, err = io.Copy(os.Stdout, zr); err !== nothing)
-        error(err)
-    end
-
-    if (err = zr.Close(); err !== nothing)
-        error(err)
-    end
-
-    println()
+    zr = Flate.NewReaderDict(b, Go.Slice(dict))
+    out = sprint(write, zr)
+    @test out == data
     #  Substitute all of the bytes in the dictionary with a '#' to visually
     #  demonstrate the approximate effectiveness of using a preset dictionary.
-    println("Substrings matched by the dictionary are marked with #:")
-    hashDict = convert(Vector{UInt8}, dict)
-    for (i) in hashDict
-        hashDict[i] = '#'
+    hashDict = Go.Slice(dict)
+    for i in eachindex(hashDict)
+        hashDict[i] = UInt8('#')
     end
 
-    zr = flate.NewReaderDict(b, hashDict)
-    if (_, err = io.Copy(os.Stdout, zr); err !== nothing)
-        error(err)
-    end
-
-    if (err = zr.Close(); err !== nothing)
-        error(err)
-    end
-
+    seekstart(b)
+    zr = Flate.NewReaderDict(b, hashDict)
+    out = sprint(write, zr)
+    # doesn't equal because we used all '#' as dictionary
+    @test out != data
 end
-#  DEFLATE is suitable for transmitting compressed data across the network.
-function Example_synchronization()
-    local wg::sync.WaitGroup
-
-    @defer wg.Wait()
-    #  Use io.Pipe to simulate a network connection.
-    #  A real network application should take care to properly close the
-    #  underlying connection.
-    rp, wp = io.Pipe()
-    #  Start a goroutine to act as the transmitter.
-    wg.Add(1)
-    Threads.@spawn begin
-        (
-            () -> begin
-                @defer wg.Done()
-                zw, err = flate.NewWriter(wp, flate.BestSpeed)
-                if err !== nothing
-                    error(err)
-                end
-
-                b = make(Vector{UInt8}, 256)
-                for (_, m) in
-                    strings.Fields("A long time ago in a galaxy far, far away...")
-                    #  We use a simple framing format where the first byte is the
-                    #  message length, followed the message itself.
-                    b[0] = UInt8(copy(b[1, :], m))
-                    if (_, err = zw.Write(b[begin:1+length(m)]); err !== nothing)
-                        error(err)
-                    end
-
-                    #  Flush ensures that the receiver can read all data sent so far.
-                    if (err = zw.Flush(); err !== nothing)
-                        error(err)
-                    end
-
-                end
-
-                if (err = zw.Close(); err !== nothing)
-                    error(err)
-                end
-
-            end
-        )()
-    end
-
-    #  Start a goroutine to act as the receiver.
-    wg.Add(1)
-    Threads.@spawn begin
-        (() -> begin
-            @defer wg.Done()
-            zr = flate.NewReader(rp)
-            b = make(Vector{UInt8}, 256)
-            while true
-                #  Read the message length.
-                #  This is guaranteed to return for every corresponding
-                #  Flush and Close on the transmitter side.
-                if (_, err = io.ReadFull(zr, b[begin:1]); err !== nothing)
-                    if err == io.EOF
-                        #  The transmitter closed the stream
-                        break
-                    end
-
-                    error(err)
-                end
-
-                #  Read the message content.
-                n = Int(b[0])
-                if (_, err = io.ReadFull(zr, b[begin:n]); err !== nothing)
-                    error(err)
-                end
-
-                print("Received %d bytes: %s\\n", n, b[begin:n])
-            end
-
-            println()
-            if (err = zr.Close(); err !== nothing)
-                error(err)
-            end
-
-        end)()
-    end
-
-end
-end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,7 @@ include("dict_decoder_test.jl")
 include("huffman_bit_writer_test.jl")
 include("inflate_test.jl")
 include("deflate_test.jl")
+include("example_test.jl")
+include("flate_test.jl")
 
 end
